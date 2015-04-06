0620eeb757206c5ebf6c10ee3e57f671be296c11
inved1 <inved1@bfh.ch>
2015-04-07 00:23:28 +0200

    huhu

--- singleProjects/Car/Assets/Scripts/CarBehaviour.cs
+++ singleProjects/Car/Assets/Scripts/CarBehaviour.cs
@@ -2,170 +2,309 @@
 using System.Collections;
 
 public class CarBehaviour : MonoBehaviour {
+	
+
+
+	
 	public WheelCollider 	wheelFL;
 	public WheelCollider 	wheelFR;
-	public WheelCollider	wheelRL;
-	public WheelCollider	wheelRR;
-	public float 			maxTorque = 500f;
-	public float 			breakTorque = 1000f;
+	public WheelCollider 	wheelRL;
+	public WheelCollider 	wheelRR;
+	public float			minTorque = 500f;
+	public float 			maxTorque = 3000f;
+	public float			normalBrake = 30000f;
+	public float			fullBrake = 40000f;
 	public GUIText 			guiSpeed;
+	public GUIText			guiRPM;
+	public Texture2D		guiRPMDisplay;
+	public Texture2D		guiRPMPointer;
 	public Texture2D        guiSpeedDisplay;
 	public Texture2D        guiSpeedPointer;
+
+	public RotatableGUIItem needleKMH;
+	public RotatableGUIItem needleRPM;
+
+	//übersetzungen engine 
+	public float[]			gears = {-10f,9f,6f,4.5f,3f,2.5f};
+	public bool 			automaticTransmission = true;
+	public int				currentGear = 1;
+
 	public GameObject		centerOfMass;
-	public float 			fullBrakeTorque = 5000f;
-	public AudioClip		brakeAudioClip;
+	public AudioClip		audioClipBrake;
+	public AudioClip		audioClipStart;
+	public AudioClip		audioClipEngineIdle;
 
 
-	public Material 		brakeLightMaterial;
-	public Material			ReverseLightMaterial;
+
+	public Material			brakeLightMaterial;
+	public Material 		backLightMaterial;
 	public GameObject		backLightL;
 	public GameObject		backLightR;
 
-	private float           _currentSpeedKMH;
-	private Rigidbody		_rigidBody;
-	private float 			_maxSpeedKMH = 100f;
-	private float 			_maxSpeedBackwardKMH = 10f;
-	private Vector3 		_localVelocity;
-	private bool			_isFullbreaking;
-	private ParticleSystem	_DustR;
-	private ParticleSystem	_DustL;
-	private AudioSource 	_brakeAudioSource;
 
-	private Material		_stdBackLightMaterial;
+
+	private float myMaxSpeedKMH = 150f;
+	private float myMaxRPM = 10000f;
+	private float myMinRPM = 800f;
+	private float myRPM = 0f;
+	private float myWheelTorque = 0f;
+
 
 	
+	private float myShiftUpRPM = 3000f;
+	private float myShiftDownRPM =1000f;
+
 	
+	private float           	myCurrentSpeedKMH;
+	private Rigidbody 			myRigidBody;
+	private float				myMotorTorque;
+
+	private WheelFrictionCurve 	myFrictionFL;
+	private WheelFrictionCurve 	myFrictionFR;
+	private WheelFrictionCurve 	myFrictionRL;
+	private WheelFrictionCurve 	myFrictionRR;
+	private float				myBrakeTorque;
+	private ParticleSystem		myParticleSystemDustFR;
+	private ParticleSystem 		myParticleSystemDustFL;
+	private ParticleSystem		myParticleSystemDustRR;
+	private ParticleSystem 		myParticleSystemDustRL;
+	private Vector3				myLocalVelocity;
+
+	private AudioSource			myAudioSourceBrake;
+	private AudioSource			myAudioSourceStart;
+	private AudioSource			myAudioSourceIdle;
+	private Material			myBackLightMaterial;
+	private AudioSource			myAudioSource;
+
+
 	
-	// Use this for initialization
-	void Start () {
-		_rigidBody = GetComponent<Rigidbody> ();
-		_rigidBody.centerOfMass = centerOfMass.transform.position - centerOfMass.transform.parent.position;
-		_DustR = GameObject.Find ("DustR").GetComponent<ParticleSystem> ();
-		_DustL = GameObject.Find ("DustL").GetComponent<ParticleSystem> ();
+	void Start ()
+	{
+		myRigidBody = GetComponent<Rigidbody> ();
+		myParticleSystemDustFR = GameObject.Find ("DustFR").GetComponent<ParticleSystem> ();
+		myParticleSystemDustFL = GameObject.Find ("DustFL").GetComponent<ParticleSystem> ();
+		myParticleSystemDustRR = GameObject.Find ("DustRR").GetComponent<ParticleSystem> ();
+		myParticleSystemDustRL = GameObject.Find ("DustRL").GetComponent<ParticleSystem> ();
 
-		_brakeAudioSource = (AudioSource)gameObject.AddComponent<AudioSource>();
-		_brakeAudioSource.clip = brakeAudioClip;
-		_brakeAudioSource.loop = true;
-		_brakeAudioSource.volume = 0.7f;
-		_brakeAudioSource.playOnAwake = true;
+		myAudioSource = GetComponent<AudioSource> ();
 
-		_stdBackLightMaterial = backLightR.GetComponent<Material>();
+		myAudioSourceBrake = (AudioSource)gameObject.AddComponent<AudioSource>();
+		myAudioSourceBrake.clip = audioClipBrake;
+		myAudioSourceBrake.loop = true;
+		myAudioSourceBrake.volume = 0.7f;
+		myAudioSourceBrake.playOnAwake = false;
 
+		myAudioSourceStart = (AudioSource)gameObject.AddComponent<AudioSource>();
+		myAudioSourceStart.clip = audioClipStart;
+		myAudioSourceStart.loop = false;
+		myAudioSourceStart.volume = 0.7f;
+		myAudioSourceStart.playOnAwake = false;
+		
+		
+		myAudioSourceIdle = (AudioSource)gameObject.AddComponent<AudioSource>();
+		myAudioSourceIdle.clip = audioClipEngineIdle;
+		myAudioSourceIdle.loop = true;
+		myAudioSourceIdle.volume = 0.7f;
+		myAudioSourceIdle.playOnAwake = false;
+		
+		myAudioSourceStart.Play ();
+
+
+		
+		myBackLightMaterial = backLightL.GetComponent<Renderer>().material;
 	}
 	
 	// Update is called once per frame constanc time per frame
-	void FixedUpdate () {
-		float speed = GetComponent<Rigidbody> ().velocity.magnitude * 3.6f;
-		
-		_localVelocity = transform.InverseTransformDirection (_rigidBody.velocity);
-		
-		bool velocityIsForeward = Vector3.Angle(transform.forward, _rigidBody.velocity) < 50f;
+	void FixedUpdate ()
+	{
+		if (myAudioSourceStart.isPlaying) {
+			return;
+		} else {
+			myAudioSource = myAudioSourceIdle;
+			myAudioSourceIdle.Play ();
+		}
+
+
+
+		myCurrentSpeedKMH = myRigidBody.velocity.magnitude * 3.6f;
+
+		myLocalVelocity = transform.InverseTransformDirection (myRigidBody.velocity);
+
+		if ((myLocalVelocity.z > 0.2) && (myAudioSourceStart.isPlaying)) {
+			myAudioSourceStart.Stop();
+			myAudioSource = myAudioSourceIdle;
+			myAudioSource.Play();
+
+		}
+
+		bool velocityIsForeward = Vector3.Angle(transform.forward, myRigidBody.velocity) < 50f;
+
+		myMotorTorque = maxTorque * Input.GetAxis("Vertical");
+		guiSpeed.text = myCurrentSpeedKMH.ToString("0") + " KMH";
+
+
+		// MAX SPEED
+		if (myCurrentSpeedKMH > myMaxSpeedKMH)
+		{
+			myRigidBody.velocity = (myMaxSpeedKMH/3.6f) * myRigidBody.velocity.normalized;
+		}
+
+
 		bool doBraking = (Input.GetAxis("Vertical") < 0 && velocityIsForeward ||
 		                  Input.GetAxis("Vertical") > 0 && !velocityIsForeward);
+
+		myWheelTorque = 0f;
 		
-		Debug.Log (string.Format ("velocityIsForeward: {0} " +
-		                          "lovalVelocity: {1} " +
-		                          "doBreaking: {2} " +
-		                          "velocityIsForeward: {3} " + "speed: {4}", 
-		                          velocityIsForeward, _localVelocity, doBraking, velocityIsForeward, speed));
+		myRPM = 0f;
+	
 		
-		
-		
-		
-		float tmpMaxTorque = maxTorque;
-		float tmpBreakTorque = breakTorque;
-		
-		if (velocityIsForeward) {
-			//driving foreward
-			if (doBraking) {
-				//braking
-				_currentSpeedKMH = Mathf.Min (speed, _maxSpeedKMH);
-				tmpBreakTorque = breakTorque * Input.GetAxis ("Vertical") * -1f;
-				tmpMaxTorque = 0f;
-			} else {
-				//accelerate
-				_currentSpeedKMH = Mathf.Min (speed, _maxSpeedKMH);
-				if (_currentSpeedKMH == _maxSpeedKMH) {
-					Debug.Log ("max speed erreicht");
-					tmpMaxTorque = 0f;
-					tmpBreakTorque = breakTorque;
-				} else {
-					tmpMaxTorque = maxTorque * Input.GetAxis ("Vertical");
-					tmpBreakTorque = 0f;
+		//rpm			umfang 2*pi*radius  *60 in der stunde (meter) /1000 = km	
+		myRPM = ((wheelFL.rpm + wheelFR.rpm) / 2 )  * 60 / 1000f; 
+		myRPM = myRPM *gears[currentGear] / 9000 * 360;
+
+		if (doBraking || FullBrake()) {
+			
+
+			if(FullBrake())
+			{
+				myBrakeTorque = fullBrake;
+			} 
+			else
+			{
+				myBrakeTorque = normalBrake;
+			}
+			
+			wheelFL.motorTorque = wheelFR.motorTorque = wheelRL.motorTorque = wheelRR.motorTorque = 0;
+			wheelFL.brakeTorque =  wheelFR.brakeTorque = wheelRL.brakeTorque = wheelRR.brakeTorque =  myBrakeTorque;
+
+			
+			// SMOKE / BRAKE AUDIO
+			if(myCurrentSpeedKMH > 5)
+			{
+				myParticleSystemDustFR.emissionRate = myParticleSystemDustFL.emissionRate = 
+					myParticleSystemDustRR.emissionRate = myParticleSystemDustRL.emissionRate = Mathf.Max(myCurrentSpeedKMH * 1.3f, 2);
+				
+				if(wheelRL.isGrounded) myParticleSystemDustRL.enableEmission = true;
+				if(wheelRR.isGrounded) myParticleSystemDustRR.enableEmission = true;
+				if(wheelFL.isGrounded) myParticleSystemDustFL.enableEmission = true;
+				if(wheelFR.isGrounded) myParticleSystemDustFR.enableEmission = true;
+				
+				if(!myAudioSourceBrake.isPlaying && wheelRL.isGrounded && wheelRR.isGrounded)
+				{
+					myAudioSourceBrake.Play();
 				}
 			}
-		} else if (!velocityIsForeward) {
-			//driving backward
-			if (!doBraking) {
-				//accelerate
-				_currentSpeedKMH = Mathf.Min (speed, _maxSpeedBackwardKMH);
-				if (_currentSpeedKMH == _maxSpeedBackwardKMH) {
-					Debug.Log ("max reverse speed erreicht");
-					tmpMaxTorque = maxTorque * Input.GetAxis ("Vertical");
-					tmpBreakTorque = 0f;
-				} else {
-					tmpBreakTorque = 0f;
-					tmpMaxTorque = breakTorque * Input.GetAxis ("Vertical");
+			else
+			{
+				myParticleSystemDustFR.enableEmission = myParticleSystemDustFL.enableEmission = 
+					myParticleSystemDustRR.enableEmission = myParticleSystemDustRL.enableEmission = false;
+				
+				if(myAudioSourceBrake.isPlaying)
+				{
+					myAudioSourceBrake.Stop();
 				}
-				
-			} else {
-				//braking
-				_currentSpeedKMH = Mathf.Min (speed, _maxSpeedBackwardKMH);
-				tmpMaxTorque = 0f;
-				tmpBreakTorque = maxTorque * Input.GetAxis ("Vertical");
-				
 			}
+			
+			backLightL.GetComponent<Renderer>().material = brakeLightMaterial;
+			backLightR.GetComponent<Renderer>().material = brakeLightMaterial;
+			
 		} else {
-			//probably standing still
+			
+
+			
+			wheelFL.brakeTorque = wheelFR.brakeTorque = wheelRL.brakeTorque = wheelRR.brakeTorque = 0f;
+			wheelFL.motorTorque = wheelFR.motorTorque = wheelRL.motorTorque = wheelRR.motorTorque = myMotorTorque;
+
+			// SMOKE
+			myParticleSystemDustFR.enableEmission = myParticleSystemDustFL.enableEmission = 
+				myParticleSystemDustRR.enableEmission = myParticleSystemDustRL.enableEmission  = false;
+			
+			// BRAKE AUDIO
+			if(myAudioSourceBrake.isPlaying)
+			{
+				myAudioSourceBrake.Stop();
+			}
+			
+			if(!velocityIsForeward)
+			{
+				backLightL.GetComponent<Renderer>().material = backLightMaterial;
+				backLightR.GetComponent<Renderer>().material = backLightMaterial;
+			}
+			else
+			{
+				backLightL.GetComponent<Renderer>().material = myBackLightMaterial;
+				backLightR.GetComponent<Renderer>().material = myBackLightMaterial;
+			}
 		}
 		
-		wheelFL.motorTorque = tmpMaxTorque;
-		wheelFL.brakeTorque = tmpBreakTorque;
-		wheelRL.motorTorque = wheelRR.motorTorque = wheelFR.motorTorque = wheelFL.motorTorque;
-		wheelRL.brakeTorque = wheelRR.brakeTorque = wheelFR.brakeTorque = wheelFL.brakeTorque;
-		
-		wheelFR.steerAngle = wheelFL.steerAngle = 10 * Input.GetAxis ("Horizontal");
+		// STEERING
+		float steerFactor = Mathf.Max (-10f / myMaxSpeedKMH * myCurrentSpeedKMH + 20f);
+		wheelFL.steerAngle = steerFactor * Input.GetAxis("Horizontal");
+		wheelFR.steerAngle = wheelFL.steerAngle;
+
+	
+		needleKMH.angle = myCurrentSpeedKMH + 40f;
+		needleRPM.angle = myRPM + 80f; 
+		Debug.Log (myRPM);
+		Debug.Log (gears [currentGear]);
+
+		//shifting
+		if (automaticTransmission && (currentGear == 1) && !velocityIsForeward) {
+			ShiftDown (); //reverse
+		} else if (automaticTransmission && (currentGear == 0) && velocityIsForeward){
+			ShiftUp(); //from reverse to forward
+		} else if (automaticTransmission && (myRPM > myShiftUpRPM) && velocityIsForeward) {
+			ShiftUp();
+		} else if (automaticTransmission && (myRPM < myShiftDownRPM) && (currentGear > 1)) {
+			ShiftDown();
+		}
 
 
-		_isFullbreaking = FullBreaking();
-		
-		
-		guiSpeed.text = _currentSpeedKMH.ToString("0");
+
 	}
 	
 	// OnGUI is called on every frame when the orthographic GUI is rendered
 	void OnGUI() 
-	{   GUI.Box(new Rect(0, 0, 140, 140), guiSpeedDisplay);
-		GUIUtility.RotateAroundPivot(Mathf.Abs(_currentSpeedKMH) + 40, new Vector2(70,70));
-		GUI.DrawTexture(new Rect(0, 0, 140, 140), guiSpeedPointer, ScaleMode.StretchToFill);
+
+	{   
+
+		GUI.Box(new Rect(0, 0, 140, 140), guiSpeedDisplay);
+
+
+		GUI.Box(new Rect(150, 0, 140, 140), guiRPMDisplay);
+
+
+
+
 	}
+	
 
 	void Update(){
 		SetAudioPitch ();
 	}
-	void SetAudioPitch(){
-		float gearSpeedDelta = 25.0f;
-		int gear = System.Math.Min ((int)(_currentSpeedKMH / gearSpeedDelta), 5);
-		float gearSpeedMin = gear * gearSpeedDelta;
-		GetComponent<AudioSource> ().pitch = (_currentSpeedKMH - gearSpeedMin) / gearSpeedDelta * 0.5f + 0.4f;
 
-
-	}
-	bool FullBreaking()
+	void SetAudioPitch()
 	{
-		if (Input.GetKey("space"))
-		{ 
-			wheelRL.brakeTorque = wheelFL.brakeTorque = wheelRL.brakeTorque = wheelRR.brakeTorque =  fullBrakeTorque;
-			_DustR.enableEmission = _DustL.enableEmission = true;
-			_brakeAudioSource.Play();
-			return true;
-		}
-		else
-		{ 
-			wheelRL.brakeTorque = wheelFL.brakeTorque = wheelRL.brakeTorque = wheelRR.brakeTorque =  0f;
-			_DustL.enableEmission = _DustR.enableEmission = false;
-			_brakeAudioSource.Stop();
-			return false;
+
+	
+	}
+	
+	
+	bool FullBrake()
+	{
+		return Input.GetKey ("space");
+	}
+
+	void ShiftUp(){
+		if (currentGear < gears.Length - 1) {
+			currentGear++;
 		}
 	}
+
+	void ShiftDown() {
+		if (currentGear > 0) {
+			currentGear--;
+		}
+	}
+
 }
