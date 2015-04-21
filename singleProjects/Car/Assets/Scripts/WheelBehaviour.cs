753b0be11dfd26931d74f41e9c0d11dd3b85e3b7
inved1 <inved1@bfh.ch>
2015-04-21 17:47:03 +0200

    comm

--- singleProjects/Car/Assets/Scripts/WheelBehaviour.cs
+++ singleProjects/Car/Assets/Scripts/WheelBehaviour.cs
@@ -4,9 +4,20 @@ using System.Collections;
 public class WheelBehaviour : MonoBehaviour {
 	public WheelCollider wheelCol; // wheel colider object
 	// Use this for initialization
-	void Start () {
+	private SkidmarkBehaviour _skidmarks; // skidmark script
+	private int _skidmarkLast; // index of last skidmark
+	private Vector3 _skidmarkLastPos;// position of last skidmark
 
+
+	void Start ()
+	{
+		// Get skidmarks script (not available in sceneMenu)
+		GameObject skidmarksGO = GameObject.Find("Skidmarks");
+		if (skidmarksGO)
+			_skidmarks = skidmarksGO.GetComponent<SkidmarkBehaviour>();
+			_skidmarkLast = -1;
 	}
+
 	// Update is called once per frame
 	void Update ()
 	{
@@ -15,6 +26,27 @@ public class WheelBehaviour : MonoBehaviour {
 		Vector3 position;
 		wheelCol.GetWorldPose(out position, out quat);
 		transform.position = position;
+
 		transform.rotation = quat;
+
+		WheelHit hit;
+		if (wheelCol.GetGroundHit(out hit))
+			DoSkidmarking(hit);
+
+
+	}
+	void DoSkidmarking(WheelHit hit)
+	{
+		// absolute velocity at wheel in world space
+		Vector3 wheelVelo = wheelCol.attachedRigidbody.GetPointVelocity(hit.point);
+		if(Input.GetKey("space"))
+		{ if (Vector3.Distance(_skidmarkLastPos, hit.point) > 0.1f)
+			{ _skidmarkLast = _skidmarks.Add(hit.point + wheelVelo*Time.deltaTime,
+				                                 hit.normal,
+				                                 0.5f,
+				                                 _skidmarkLast);
+				_skidmarkLastPos = hit.point;
+			}
+		} else _skidmarkLast = -1;
 	}
 }
